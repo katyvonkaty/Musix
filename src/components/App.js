e33a4ff9954f788063194f2d0aac1a63ae48687b
katyvonkaty <kj0203@gmail.com>
2020-10-07 12:56:45 -0400

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import SearchBar from './SearchBar';
-// import axios from "axios";
-//
-//
-// class App extends React.Component{
-//
-//   async onSearchSubmit(term){
-//     const response = await axios.get("https://api.adviceslip.com/advice/search/" + term)
-//     console.log(response.data.slips)
-//   }
-//
-//   render(){
-//     return <div className="ui container" style={{marginTop:"15px"}}>
-//       <SearchBar onSubmit={this.onSearchSubmit} />
-//     </div>
-//   }
-// }
-//
-// export default App;
-//
-
-
 import React from 'react';
 import SearchBar from './SearchBar';
 import axios from "axios";
@@ -28,8 +5,8 @@ import axios from "axios";
 
 class App extends React.Component{
 
-  onSearchSubmit(term) {
-    axios
+  async onSearchSubmit(term) {
+    const response = await axios
     .get ("https://shazam.p.rapidapi.com/auto-complete?locale=en-US&term=" + term, {
     params: {query: term},
     headers: {
@@ -37,10 +14,7 @@ class App extends React.Component{
       "x-rapidapi-key": "70aa9d5184msh24f489409866b23p1ff84cjsn5832992d4f80"
     }
   })
-  .then(response => {
-  	console.log(response.data);
-  })
-
+  	console.log(response.data.hints);
 }
 
   render(){
